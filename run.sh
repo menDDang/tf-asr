3ffa404ed840bd562ee6acd7f8ef10fda0cba31d
윤기무 <kmyoon@mediazen.co.kr>
2021-10-03 18:02:49 +0900

    add chkpt & log directory

--- run.sh
+++ run.sh
@@ -1,18 +1,33 @@
 #!/bin/bash
 
-#data_dir=/home/kmyoon/data/LibriSpeech
-#work_dir=/home/kmyoon/exp/tf-asr
-#out_dir=${work_dir}/outputs/librispeech
+data_dir=/home/kmyoon/data/LibriSpeech
+work_dir=/home/kmyoon/exp/tf-asr
 
-data_dir=/home/feesh/corpus/LibriSpeech
-work_dir=/home/feesh/projects/tf-asr
+#data_dir=/home/feesh/corpus/LibriSpeech
+#work_dir=/home/feesh/projects/tf-asr
+
 out_dir=${work_dir}/outputs/librispeech
 mkdir -p ${out_dir}
 
 config=${work_dir}/configs/librispeech_conformer_v1.yml
 
-python3 prepare_librispeech.py \
-    --data_dir ${data_dir} \
-    --out_dir ${out_dir} \
+train_tfrecord=${out_dir}/train.tfrecord
+valid_tfrecord=${out_dir}/valid.tfrecord
+
+# Data preparation
+if [ ! -f ${train_tfrecord} ] || [ ! -f ${valid_tfrecord} ]; then
+    python3 prepare_librispeech.py \
+        --data_dir ${data_dir} \
+        --out_dir ${out_dir} \
+        --config ${config} \
+        || exit 1
+fi
+
+# Train
+python3 temp.py \
+    --train_tfrecord ${valid_tfrecord} \
+    --valid_tfrecord ${valid_tfrecord} \
     --config ${config} \
+    --log_dir ${out_dir}/logs \
+    --chkpt_dir ${out_dir}/chkpts \
     || exit 1
